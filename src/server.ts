cb36d03cd736c26b518312b338cb6d877ed75465
Fahim Muntashir <fahimasdf07@gmail.com>
2025-12-31 15:05:10 +0600

    basic setup done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 const PORT = process.env.PORT || 3000;
 
-function server() {
+async function server() {
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    console.log(error);
+
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+
   app.listen(PORT, () => {
-    console.log("server is running on port ", PORT);
+    console.log("server is running on port :", PORT);
   });
 }
 
